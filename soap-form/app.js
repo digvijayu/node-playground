79dce7a6adb60a0dd722cf0276f3257626dada8c
Digvijay Upadhyay <digvijay.upadhyay@roxorgaming.com>
2022-04-23 22:12:54 +0100

    chore: added user REST apis

--- soap-form/app.js
+++ soap-form/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 
+let USERS = {};
+
 
 app.use(express.json({
   type: ['application/json', 'text/plain']
@@ -19,14 +21,79 @@ app.get('/', function (req, res) {
 app.post('/login', function (req, res) {
   console.log('req', req.headers);
   console.log('req', req.body);
-  res.send(JSON.stringify({
-    "success": true,
-    message: `Welcome user ${req.body.name}`
-  }))
-})
 
-app.get('/login', function (req, res) {
-  res.send('Get login is not allowed')
-})
 
-app.listen(3000)
+  if (req.body.name === "Divya") {
+    res.send(JSON.stringify({
+      "success": true,
+      message: `Welcome user ${req.body.name}`,
+      name: req.body.name
+    }))
+  }
+  else {
+    res.send(JSON.stringify({
+      "success": false,
+      message: `${req.body.name} is not a valid name.`
+    }))
+  }
+
+
+})
+// ---------------
+
+app.get('/user', function (req, res) {
+  res.send(JSON.stringify(Object.values(USERS)))
+});
+
+app.get('/user/:id', function (req, res) {
+  const id = req.params.id;
+
+  if (USERS[id]) {
+    res.send(JSON.stringify(USERS[id]))
+  }
+  else {
+    res.status(404).send();
+  }
+});
+
+app.post('/user', function (req, res) {
+  const id = +(new Date());
+  const { username, email } = req.body;
+
+  const user = {
+    id,
+    username,
+    email
+  }
+
+  USERS[id] = user;
+  res.send(JSON.stringify(user))
+});
+
+app.put('/user/:id', function (req, res) {
+  const id = req.params.id;
+  const user = USERS[id];
+
+  if (!user) {
+    res.status(404);
+    return;
+  }
+
+  user.username = req.body.username;
+  user.email = req.body.email;
+  res.send(JSON.stringify(user));
+});
+
+app.delete('/user/:id', function (req, res) {
+  const id = req.params.id;
+
+  if (!USERS[id]) {
+    res.status(404).send();
+    return;
+  }
+
+  delete USERS[id];
+  res.send(JSON.stringify({ successful: true }));
+});
+
+app.listen(3001)
